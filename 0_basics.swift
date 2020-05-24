ab2437b8d959a29877bd16255d69f2dbcbd680fe
Mario Inga <mario21ic@gmail.com>
2020-05-24 11:27:53 -0500

    Ready

---
+++ 0_basics.swift
@@ -0,0 +1,53 @@
+// MARK: Constants
+let pi_implicito = 3.141516
+print("pi implicito: \(pi_implicito)")
+
+let pi_explicito:Double = 3.141516
+print("pi explicito: \(pi_explicito)")
+
+let pi_explicit:Double
+pi_explicit = 3.141516
+print("pi explicit: \(pi_explicit)")
+
+
+// MARK: Variables
+var saldo_implicito = 234.45
+print("saldo implicito: \(saldo_implicito)")
+
+var saldo_explicito:Double; saldo_explicito = 234.45
+print("saldo explicito: \(saldo_explicito)")
+
+// Declaring multiple constants and variables
+let x=0.0, y=0.0, z=0.0
+var saldo1 = 123, saldo2 = 123.3, saldo3 = 123.34
+
+
+// MARK: Type Annotations
+var welcome: String = "Welcome to the "
+welcome += "playground"
+print("welcome: \(welcome)")
+welcome = "Bonjour"
+print("welcome: \(welcome)")
+
+var red, green, blue: Double
+var myFloat:Float = 12.45
+
+var temperature:Double = 12.3
+var edad:Int = 34
+
+// MARK: Naming Constants and Variables
+// Can contain almost any character, including Unicode characters
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow"
+
+// MARK: Signed and Unsigned
+var myInt8:Int8 = -12
+var myInt16:Int16 = -12
+var myInt32:Int32 = -12
+var myInt64:Int64 = -12
+
+var myUInt8:UInt8 = 12
+var myUInt16:UInt16 = 12
+var myUInt32:UInt32 = 12
+var myUInt64:UInt64 = 12
---
+++ 1_basic_operators.swift
@@ -0,0 +1,16 @@
+//  AprendoSwift
+//
+//  Created by Mario J. Inga Cahuana on 5/12/20.
+//
+
+// MARK: Basic Operators
+
+let b = 10
+var a = 5
+
+let (x, y) = (1, 2)
+
+if let x:Int = a {
+  print("Asignado \(x)")
+}
+
